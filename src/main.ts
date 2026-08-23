306158529d6f88d535e368ef19327af159c58f30
dario villegas <rdario_v@hotmail.com>
2026-08-23 09:13:32 -0300

    install and configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  
+  const config = new DocumentBuilder()
+    .setTitle('Trellium API')
+    .setDescription('API documentation for Trellium')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
